e430e4e77efba12e95336bd929e80dc4a51af573
Chris Casey <ccasey@guidebook.com>
2018-12-08 15:36:35 -0800

    Initial commit

---
+++ category_parser.py
@@ -0,0 +1,18 @@
+
+
+def main():
+	filepath = 'categories.txt'  
+	raw_lines = []
+	with open(filepath) as fp:  
+		line = fp.readline()
+		while line:
+			raw_lines.append(line)
+			line = fp.readline()
+	for line in raw_lines:
+		line_1 = line.split('(')
+		line_2 = line_1[1].split(",")
+		print line_2[0]
+
+
+if __name__ == "__main__":
+    main()
---
+++ crf.py
@@ -0,0 +1,63 @@
+# from yelp.client import Client
+# MY_API_KEY = "qlWvJAEpa42CLh0Lz7vBnF-JkIsngtx0mlQSqqf4t1fG5SOXOPOJGOHyGpkV8EbRLovqiyBsd-nGO_UPfQx_B5gErZP89n4pREobk8kCGkR7K1htwoX0L5LZtzcMXHYx"
+# client = Client(MY_API_KEY)
+import requests
+import json
+
+API_KEY = 'Bearer qlWvJAEpa42CLh0Lz7vBnF-JkIsngtx0mlQSqqf4t1fG5SOXOPOJGOHyGpkV8EbRLovqiyBsd-nGO_UPfQx_B5gErZP89n4pREobk8kCGkR7K1htwoX0L5LZtzcMXHYx'
+CUSTOM_API_HEADER = {'Authorization': API_KEY}
+BUSINESS_SEARCH_BASE_URL = 'https://api.yelp.com/v3/businesses/search'
+SEARCH_LOC = 'Berkeley, CA'
+PRICE = "1, 2, 3"
+CATEGORIES_ADVENTUROUS = 'categories-adventurous.txt'
+
+def find_restaurants(categories):
+	payload = {'location': SEARCH_LOC, 'limit': 3, 'sort_by': 'rating', 'open_now': True, 'categories': categories, 'price': PRICE}
+	r = requests.get(BUSINESS_SEARCH_BASE_URL, params=payload, headers=CUSTOM_API_HEADER)
+	if not r.status_code is 200:
+		print r.error
+		return
+	response_str = r.content
+	businesses = generate_yelp_restaurants(json.loads(response_str))
+	print businesses
+	
+
+def generate_yelp_restaurants(response_json):
+	businesses = []
+	for business in response_json['businesses']:
+		name = business.get('name', None)
+		rating = business.get('rating', None)
+		price = business.get('price', None)
+		address = business.get('address', None)
+		categories = ', '.join([str(c['title']) for c in business['categories']])
+		businesses.append(YelpRestaurant(name, rating, price, categories, address))
+	return businesses	
+
+def enrich_categories():
+	categories = []
+	with open(CATEGORIES_ADVENTUROUS) as fp:  
+		line = fp.readline()
+		while line:
+			categories.append(line.rstrip())
+			line = fp.readline()
+	return ",".join(categories)		
+
+
+class YelpRestaurant:
+	def __init__(self, name, rating, price, categories, address):
+		self.name = name
+		self.rating = rating
+		self.price = price
+		self.categories = categories
+		self.address = address
+
+	def ___str___(self):
+		return self.name	
+
+	def __repr__(self):
+		return 'YelpRestaurant(name=%s, rating=%s, categories=%s)' % (self.name, self.rating, self.categories)
+
+if __name__ == "__main__":
+	categories = enrich_categories()
+	find_restaurants(categories)
+
